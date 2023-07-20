134a739c42cedb86e48f5cb5f13d398445386cca
seamus88 <a-semenov-88@yandex.ru>
2023-07-20 19:18:43 +0300

    добавил скриншот и коммент с номером когорты

--- create_order_test.py
+++ create_order_test.py
@@ -1,6 +1,8 @@
 import data
 import sender_stand_request
 
+# Андрей Семенов 6 когорта - финальный проект, инженер по тестированию плюс
+
 # тест создания заказа и получения информации по нему по номеру трека
 def test_create_order():
     req = sender_stand_request.create_order(data.headers, data.order_body)
